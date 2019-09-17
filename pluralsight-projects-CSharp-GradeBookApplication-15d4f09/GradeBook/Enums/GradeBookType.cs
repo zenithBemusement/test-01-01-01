dcff70ea6094eecbb78912cf0828d4fde5597c9a
zenithBemusement <50882751+zenithBemusement@users.noreply.github.com>
2019-09-17 16:44:40 -0500

    Add files via upload

---
+++ pluralsight-projects-CSharp-GradeBookApplication-15d4f09/GradeBook/Enums/GradeBookType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.Enums
+{
+    public enum GradeBookType
+    {
+        Standard,
+        Ranked,
+        ESNU,
+        SixPoint
+    }
+}
